7472960995f6d74f75e3865962170b8a0771866c
chen <951849311@qq.com>
2018-03-29 09:40:06 +0800

    the second modified

--- wordcloud/learn english wordcloud.py
+++ wordcloud/learn english wordcloud.py
@@ -4,19 +4,22 @@ Created on Wed Mar 28 19:30:41 2018
 
 @author: DELL
 """
-from os import path
+
 from PIL import Image
 import numpy as np
 from wordcloud import WordCloud,STOPWORDS
 import matplotlib.pyplot as plt
 
-f = open(r'e:\py_data\best.txt','r').read()
+f = open(r'e:\py_data\best.txt',encoding='gbk',errors='ignore').read()
 
 alice_mask = np.array(Image.open(r'e:\py_data\alice.png'))
-stopwords = set(STOPWORDS)
-stopwords.add('python')
-
-wordcloud = WordCloud(background_color='white',mask=alice_mask,width=1000,height=860,margin=2,stopwords=stopwords).generate(f)
+stopwords = set(STOPWORDS) 
+stopwords.add('language')
+#font_path = 'E:\python_git\wordcloud\simkai.ttf'
+wordcloud = WordCloud(background_color='white',
+                      #font_path = font_path,
+                      mask=alice_mask,
+                      width=1000,height=860,margin=2,stopwords=stopwords).generate(f)
 
 plt.imshow(wordcloud)
 plt.axis('off')
